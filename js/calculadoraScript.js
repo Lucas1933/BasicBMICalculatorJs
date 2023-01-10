d522b447baf36e806d3ff0ceed931646e06489a0
Lucas David Fernandez <1933.lucas.fernandez@gmail.com>
2023-01-10 14:59:16 -0300

    codigo ordenando, se puede mejorar

--- js/calculadoraScript.js
+++ js/calculadoraScript.js
@@ -1,11 +1,71 @@
-/* botones radio de sexo */
+class Persona {
+  constructor() {
+    this.peso = document.getElementById("peso").value;
+    this.altura = document.getElementById("altura").value;
+    this.edad = document.getElementById("edad").value;
+    this.sexo = sexoSeleccionado;
+    this.nivelDeActividad = nivelDeActividad;
+  }
+}
+/**
+ *Class CalculadoraMetabolica, se encarga de calcular las calorias y
+ *validar los valores.
+ */
+class CalculadoraMetabolica {
+  constructor() {}
+
+  /* Fórmula de Mifflin 
+   hombres: (10 x peso en kg) + (6,25 x altura en cm) - (5 x edad en años) + 5
+   mujeres: (10 × peso en kg) + (6,25 x altura en cm) - (5 × edad en años) - 161
+ */
+  obtenerCalorias(persona) {
+    let resultado;
+    if (this.validateValues(persona)) {
+      let valorPorSexo = 5;
+      if (persona.sexo === "femenino") {
+        valorPorSexo = -161;
+      }
+      resultado =
+        10 * parseInt(persona.peso) +
+        6.25 * parseInt(persona.altura) -
+        5 * parseInt(persona.edad) +
+        valorPorSexo;
+    } else {
+      resultado = NaN;
+    }
+    return resultado * parseFloat(persona.nivelDeActividad);
+  }
+
+  validateValues(persona) {
+    if (
+      isNaN(persona.peso) ||
+      isNaN(persona.altura) ||
+      isNaN(persona.edad) ||
+      persona.peso <= 0 ||
+      persona.altura <= 0 ||
+      persona.edad <= 0
+    ) {
+      return false;
+    } else {
+      return true;
+    }
+  }
+}
+/* M o F radio input */
 const sexo = document.querySelectorAll("input[type=checkbox]");
-/* se lo deja hardcodeado para no tener que controlar si fue seleccionado */
-sexo[0].checked = true;
-sexo.forEach((radio) => {
-  radio.addEventListener("click", setCheckBoxSelection, false);
-});
-let sexoSeleccionado = "masculino";
+let sexoSeleccionado;
+setRadioSexoDefault();
+setListenerForRadios();
+
+function setRadioSexoDefault() {
+  sexo[0].checked = true;
+  sexoSeleccionado = "masculino";
+}
+function setListenerForRadios() {
+  sexo.forEach((radio) => {
+    radio.addEventListener("click", setCheckBoxSelection, false);
+  });
+}
 /**
  * Deja seleccionar solo un input radio (M || F).
  * Asigna al final a sexoSeleccionado el id del input seleccionado ("masculino" || "femenino").
@@ -19,15 +79,19 @@ function setCheckBoxSelection(event) {
   event.target.checked = true;
   sexoSeleccionado = event.target.id;
 }
+/* M o F radio input */
 
 /* seleccion y animacion de niveles de actividad listeners */
 const inputsNivelDeActividad = document.getElementsByName("nivelDeActividad");
-inputsNivelDeActividad.forEach((boton) => {
-  boton.addEventListener("click", setNivelDeActividad, false);
-  boton.addEventListener("click", setNivelDeActividadBotonEstilo, false);
-});
-
 let nivelDeActividad;
+setInputActividadListener();
+
+function setInputActividadListener() {
+  inputsNivelDeActividad.forEach((boton) => {
+    boton.addEventListener("click", setNivelDeActividad, false);
+    boton.addEventListener("click", setNivelDeActividadBotonEstilo, false);
+  });
+}
 /**
  * Asigna el value del boton de nivel de actividad seleccionado ("1.34, 1.75, etc")
  * @param {Event} event el boton de nivel de actividad que dispara el evento.
@@ -75,122 +139,53 @@ function setNivelDeActividadBotonAnimation(etiquetaSeleccionada) {
     false
   );
 }
+/* seleccion y animacion de niveles de actividad listeners */
 
-/**
- * funcion main que se ejecuta al presionar "calcular"
- * da pie a la creacion de un objeto Persona con los datos obtenidos del form
- * (peso, altura, etc)
- * se crea un objeto calculadora y se le pasa al metodo de la misma obtenerCalorias()
- * la persona creada.
- * @return {void}.
- */
+/* Calculo*/
 const botonCalcular = document.getElementById("calcularBoton");
-botonCalcular.addEventListener("click", main, false);
+botonCalcular.addEventListener("click", main);
 function main() {
-  const persona = new Persona();
+  let persona = new Persona();
   const calculadora = new CalculadoraMetabolica();
-  calculadora.obtenerCalorias(persona);
-}
-
-// eslint-disable-next-line require-jsdoc
-class Persona {
-  // eslint-disable-next-line require-jsdoc
-  constructor() {
-    this.peso = document.getElementById("peso").value;
-    this.altura = document.getElementById("altura").value;
-    this.edad = document.getElementById("edad").value;
-    this.sexo = sexoSeleccionado;
-    this.nivelDeActividad = nivelDeActividad;
-  }
+  let resultado = calculadora.obtenerCalorias(persona);
+  appendResult(resultado);
 }
 /**
- *Class CalculadoraMetabolica, se encarga de calcular las calorias
- *validar los valores y mostrar el resultado del calculo.
+ *accede a los elementos del cartel, les da estilo y appends el resultado.
+ * @param {floar} resultado
  */
-class CalculadoraMetabolica {
-  /**
-   * constructor por defecto
-   */
-  constructor() {}
-  /**
-   *recibe una persona y valida que los datos sean numericos y mayores a cero
-   *de ser invalidados llama appendDatoinvalido()
-   *de otra forma devuelve true
-   * @param {Persona} persona
-   * @return {boolean}
-   */
-  validateValues(persona) {
-    if (
-      isNaN(persona.peso) ||
-      isNaN(persona.altura) ||
-      isNaN(persona.edad) ||
-      persona.peso <= 0 ||
-      persona.altura <= 0 ||
-      persona.edad <= 0
-    ) {
-      this.appendDatoinvalido();
-      return false;
-    } else {
-      return true;
-    }
-  }
-  /**
-   *añade al div que contiene el cartel para mostrar el resultado
-   *informando que uno de los valores es invalido
-   *finalmente remueve la class hidden mostrando asi el cartel
-   * @return {void}
-   */
-  appendDatoinvalido() {
-    const cartelResultado = document.body.querySelector(
-      "div:first-child" /* primer hijo del body, el div que es el cartel con el resultado */
-    );
-    const caloriasQuemadasPorDia = cartelResultado.firstElementChild;
-    const resultadoText = cartelResultado.lastElementChild;
-    caloriasQuemadasPorDia.innerText = " ";
-    resultadoText.style.color = "#FF4747";
-    resultadoText.innerText = "Uno de los valores ingresados es invalido";
-    cartelResultado.classList.remove("hidden");
-  }
-  /**
-   *utilizando la formula mifflin st jeor se calcula utilizando los valores
-   *de la persona
-   *retorna el resultado multiplicado por el nivel de actividad
-   * @param {Persona} persona
-   * @return {float}
-   */
-  obtenerCalorias(persona) {
-    if (this.validateValues(persona)) {
-      let resultado;
-      let valorPorSexo = 5;
-      if (persona.sexo === "femenino") {
-        valorPorSexo = -161;
-      }
-      resultado =
-        10 * parseInt(persona.peso) +
-        6.25 * parseInt(persona.altura) -
-        5 * parseInt(persona.edad) +
-        valorPorSexo;
-      this.appendResult(resultado * parseFloat(persona.nivelDeActividad));
-      return resultado * parseFloat(persona.nivelDeActividad);
-    }
-  }
-  /**
-   *accede a los elementos del cartel, les da estilo y appends el resultado.
-   * @param {floar} resultado
-   */
-  appendResult(resultado) {
+function appendResult(resultado) {
+  if (!isNaN(resultado)) {
     let cartelResultado = document.body.querySelector("div:first-child");
     let caloriasQuemadasPorDia = cartelResultado.firstElementChild;
     let resultadoText = cartelResultado.lastElementChild;
     resultadoText.style.color = "white";
 
-    if (!isNaN(resultado)) {
-      caloriasQuemadasPorDia.innerText = "Calorias quemadas por dia:";
-      resultadoText.innerText = parseInt(resultado) + " Kl";
-      cartelResultado.classList.remove("hidden");
-    }
+    caloriasQuemadasPorDia.innerText = "Calorias quemadas por dia:";
+    resultadoText.innerText = parseInt(resultado) + " Kl";
+    cartelResultado.classList.remove("hidden");
+  } else {
+    appendDatoinvalido();
   }
 }
+/**
+ *añade al div que contiene el cartel para mostrar el resultado
+ *informando que uno de los valores es invalido
+ *finalmente remueve la class hidden mostrando asi el cartel
+ * @return {void}
+ */
+function appendDatoinvalido() {
+  const cartelResultado = document.body.querySelector(
+    "div:first-child" /* primer hijo del body, el div que es el cartel con el resultado */
+  );
+  const caloriasQuemadasPorDia = cartelResultado.firstElementChild;
+  const resultadoText = cartelResultado.lastElementChild;
+  caloriasQuemadasPorDia.innerText = " ";
+  resultadoText.style.color = "#FF4747";
+  resultadoText.innerText = "Uno de los valores ingresados es invalido";
+  cartelResultado.classList.remove("hidden");
+}
+/* Calculo */
 
 /* cuadro de dialogo sobre los niveles de actividad */
 let iconoDeAyuda = document.getElementById("helpIcon");
@@ -216,3 +211,4 @@ function abrirCuadroDeDialogo(event) {
 function cerrarCuadroDeDialogo(event) {
   cuadroDeDialogoNdeActividad.classList.add("hidden");
 }
+/* cuadro de dialogo sobre los niveles de actividad */
