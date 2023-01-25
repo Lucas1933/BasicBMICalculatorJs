94aaa3ba4bfe5b136c6f94b849e58b57c763a11c
Lucas David Fernandez <1933.lucas.fernandez@gmail.com>
2023-01-25 19:37:10 -0300

    listo para presentar, no conforme con el resultado igualmente

---
+++ js/APIexercises/showExercises.js
@@ -0,0 +1,67 @@
+async function fetchExercises() {
+  let ejercicios = [];
+  let tire = await fetch(
+    "https://api.api-ninjas.com/v1/exercises?muscle=lats",
+    {
+      method: "GET",
+      headers: { "X-Api-Key": "SdCemAtVyQb1kcwD/dIkhw==a8c3wP24oCVaaZNm" },
+      contentType: "application/json",
+    }
+  ).then((respuesta) => respuesta.json());
+  let empuje = await fetch(
+    "https://api.api-ninjas.com/v1/exercises?muscle=chest",
+    {
+      method: "GET",
+      headers: { "X-Api-Key": "SdCemAtVyQb1kcwD/dIkhw==a8c3wP24oCVaaZNm" },
+      contentType: "application/json",
+    }
+  ).then((respuesta) => respuesta.json());
+  let piernas = await fetch(
+    "https://api.api-ninjas.com/v1/exercises?muscle=quadriceps",
+    {
+      method: "GET",
+      headers: { "X-Api-Key": "SdCemAtVyQb1kcwD/dIkhw==a8c3wP24oCVaaZNm" },
+      contentType: "application/json",
+    }
+  ).then((respuesta) => respuesta.json());
+  let cardio = await fetch("https://api.api-ninjas.com/v1/exercises?cardio", {
+    method: "GET",
+    headers: { "X-Api-Key": "SdCemAtVyQb1kcwD/dIkhw==a8c3wP24oCVaaZNm" },
+    contentType: "application/json",
+  }).then((respuesta) => respuesta.json());
+  ejercicios.push(tire, empuje, piernas, cardio);
+  return ejercicios;
+}
+
+export async function showExercises() {
+  let ejercicios = [];
+  ejercicios = await fetchExercises();
+  let cardio = ejercicios.pop();
+  let piernas = ejercicios.pop();
+  let empuje = ejercicios.pop();
+  let tire = ejercicios.pop();
+  let randomIndex = Math.floor(
+    Math.random() * (10 - 0 + 1) + 0
+  ); /* min 0 max 10 */
+  let div = document.getElementById("ejercicios");
+  div.classList.add("flex", "grow", "w-[%35]", "justify-evenly");
+  div.innerHTML = " ";
+  div.innerHTML = ` 
+  <div>
+    <p>Ganar musculo</p>
+    <ul>
+      <li>${tire[randomIndex].name}</li>
+      <li>${empuje[randomIndex].name}</li>
+      <li>${piernas[randomIndex].name}</li>
+    </ul>
+  </div>
+  <div>
+    <p>Perder grasa</p>
+    <ul>
+      <li>${cardio[randomIndex].name}</li>
+      <li>${cardio[0].name}</li>
+      <li>${cardio[6].name}</li>
+    </ul>
+  </div>
+`;
+}
--- js/calculadoraScript.js
+++ js/calculadoraScript.js
@@ -1,12 +1,12 @@
 import { showResult } from "./cartel/cartelResultado.js";
 import { printHistorial } from "./historial/historial.js";
 import { setCartelAyudaListeners } from "./cartel/cartelAyuda.js";
-
+import { showExercises } from "./APIexercises/showExercises.js";
+setCartelAyudaListeners();
 const btnCalcular = document.getElementById("calcularBoton");
 btnCalcular.addEventListener("click", exeCalculo);
-
 function exeCalculo() {
   showResult();
   printHistorial();
+  showExercises();
 }
-setCartelAyudaListeners();
